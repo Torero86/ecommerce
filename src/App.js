c0b97e977093d28bbaaba544e495a06f9441d0ff
manuelovejeroe <manuelovejeroetchegaray@gmail.com>
2023-12-03 20:09:43 -0300

    ultimoDetalleDeProducto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
+import ItemDetailContainer  from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
         <ItemListContainer greeting={"Bienvenidos!"}/>
-        <ItemCount initial={1} stock={10} onAdd={(quantity) =>console.log ("cantidad agregada", quantity) } />
+        <ItemDetailContainer/>
         
 
 
